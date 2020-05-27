9a5031b5294c8beec261096a7377ce11a0c3a087
Sandu Cristian <cristi.sandu.98@gmail.com>
2020-05-28 00:41:27 +0300

    added file read & parser

--- MainLP.pl
+++ MainLP.pl
@@ -1,12 +1,50 @@
-unificareTermeni(JSParamInput, Propozitie, JSDataKB, NRTermeniLipsea) :- 
-    
+% am ajuns la final
+unificareTermeni(
+    _,  % dictionarul unde completam parametrii
+    _,  % parcurgerea popozitiei
+    [_ | JSDataKB]   % knowledge base-ul
+) :- 
+    % check if is final exists
+    get_dict('_isFinal', JSDataKB, _)
+.
+
+% cand trebuie mers pe primul dictionar
+unificareTermeni(
+    JSParamInput,   % dictionarul unde completam parametrii
+    [HProp|TProp],  % parcurgerea popozitiei
+    [JSDataKBOriginal | JSDataKB]   % knowledge base-ul
+) :- 
+    % transformare string in atom
+    string(HProp), atom_string(Atom, HProp), 
+    unificareTermeni(JParamInput, TProp, [JSDataKBOriginal | JSDataKBOriginal])
+.
+
+unificareTermeni(
+    JSParamInput,   % dictionarul unde completam parametrii
+    [HProp|TProp],  % parcurgerea popozitiei
+    [JSDataKBOriginal | JSDataKB]  % knowledge base-ul
+) :- 
+    % scoatem datele din dictionar
+    atom_string(Atom, HProp), get_dict(Atom, JSDataKB, Termen), % scoatem termenii din dictionar
+
+    % apelare recursiva
+    unificareTermeni(JParamInput, TProp, [JSDataKBOriginal | Termen])
 .
 
 % analizam json-ul primit
-analyseData(JSDataInput, JSDataKB) :-
+analyseData(JSData, JSDataKB) :-
     get_dict(actiune, JSData, Actiune), % obtinem actiunea
     get_dict(parametrii, JSData, Parametrii), % obtinem parametrii
-    get_dict(propozitie, JSData, Propozitie), % obtinem propozitie
+
+    % obtinem datele din parametrii
+    get_dict(propozitie, Parametrii, Propozitie), % obtinem propozitie
+
+    % compunem argumentele pentru parsare
+    split_string(Propozitie, " ", " ", SplitPropozitie), % impartim propozitia in termeni
+    append([Actiune], SplitPropozitie, CompletPropozitie), % adaugam si actiunea la cautare
+
+    % incepem unificarea termenilor
+    unificareTermeni(Params, CompletPropozitie, [JSDataKB | JSDataKB]) % parcurgem propozitia si completam dictionarul
 .
 
 % read json data
@@ -17,8 +55,8 @@ readJson(InputFile, JSData) :-
 .
 
 % entryPoint
-entryPoint(H) :-
-    readJson("./Input.txt", _), % citi
+entryPoint(_) :-
+    readJson("./Input.txt", JSData), % citi
     readJson("./KBase.txt", JSDataKB),
-    analyseData(JSData, JSDataKB)
+    analyseData(JSData, JSDataKB) % obtinem actiunea
 .
---
+++ README.md
@@ -0,0 +1,16 @@
+# Natural Language Intent Detection
+
+Def: NLID este o soluție care să asocieze propoziția primită la intrare cu toate posibilitățile din setul cunoscut 
+(adnotăm elementele cunoscute cu etichete).
+Permite un număr arbitrar de entități absente (momentan 1).
+
+Avantajele acestei metode:
+1. Ușurință: Permite efectuarea operațiilor CRUD asupra elementelor din setul cunoscut fără modificări de cod.
+  eg: adaugă eveniment între ora X și Y / adaugă evenimen la ora X sau Y
+2. Scalabilitate: Se pot adăuga un număr arbitrar de etichete. 
+3. Personalizare: Se pot specifica propietăți pentru etichete.
+4. Simplitatea înterținerii: Adăugarea de noi propoziții se face la nivel de bază de cunoștiințe, fără ajustarea
+  codului
+5. Soluție completă: Determină toate soluțiile.
+6. Eficiență echilibrată: Mai puțină memorie decât un ROM, mai puțin timp decât bruteforce.
+7. Procesare bazată pe LCA: Propozițiile cu prefix comun sunt specificate o singură dată.
