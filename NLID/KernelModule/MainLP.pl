9e4d8a837f5c0f2048c0259bf370c05ad7e3f198
Sandu Cristian <cristi.sandu.98@gmail.com>
2020-06-14 01:01:48 +0300

    kernel parser

--- NLID/KernelModule/MainLP.pl
+++ NLID/KernelModule/MainLP.pl
@@ -2,7 +2,7 @@
 % @ - variabila
 
 % ERROR CODES
-% ERROR_001 - Premature Ending
+% ERROR_001 - Premature ending
 
 % functii generice generate
 % @arguments: Find is atom
@@ -32,11 +32,81 @@ completeDictionary(
     % incercam sa luam datele din input
     get_dict(AtomKey, JSInput, Termen) 
         -> put_dict(AtomKey, InDictionary, Termen, OutDictionary);
+    
+    % incercam sa luam datele din input
+    atom_string(AtomKey, Key), 
 
     % nu este in input, luam default-ul
     get_dict(AtomKey, JSDataKB, InsideBranch),
+
     get_dict('_default', InsideBranch, DefaultValue) ->
         put_dict(AtomKey, InDictionary, DefaultValue, OutDictionary)
+.
+
+unificareTermeni(
+    [HProp|TProp],  % parcurgerea popozitiei
+    [JSDataKBOriginal | JSDataKB],  % knowledge base-ul
+    JSDataInput,  % json-ul citit la input
+    OutDictionary  % dictionarul final cu rezultatele
+) :- 
+    % verificam sa fie dictionar
+    \+ string(JSDataKB),
+
+    % scoatem datele din dictionar
+    atom_string(Atom, HProp), get_dict(Atom, JSDataKB, Termen), % scoatem termenii din dictionar
+    
+    % daca nu este sub-arbore, incepem din root
+    string(Termen),
+
+    % scoatem din root primul termen
+    atom_string(RootAtom, Termen),
+    get_dict(RootAtom, JSDataKBOriginal, RootBranch), % scoatem termenii din dictionar
+
+    % reincepem parcurgerea din root
+    unificareTermeni(TProp, [JSDataKBOriginal | RootBranch], JSDataInput, InDictionary),
+
+    % adaugam date in dictionar
+    completeDictionary(HProp, JSDataKB, JSDataInput, InDictionary, OutDictionary)
+.
+
+unificareTermeni(
+    [HProp|TProp],  % parcurgerea popozitiei
+    [JSDataKBOriginal | JSDataKB],  % knowledge base-ul
+    JSDataInput,    % json-ul citit la input
+    OutDictionary    % dictionarul final cu rezultatele
+) :- 
+    % verificam sa fie dictionar
+    \+ string(JSDataKB),
+
+    % scoatem datele din dictionar
+    atom_string(Atom, HProp), get_dict(Atom, JSDataKB, Termen), % scoatem termenii din dictionar
+
+    % mergem pe ramura aceasta
+    unificareTermeni(TProp, [JSDataKBOriginal | Termen], JSDataInput, InDictionary),
+
+    % verificam daca este variabila
+    % incepe cu @[cuvant]
+    completeDictionary(HProp, JSDataKB, JSDataInput, InDictionary, OutDictionary),
+    ! % oprim recursivitatea
+.
+
+% termenul lipseste din KBase
+% verificam toti termenii sa vedem daca este prezent _canAbsent
+unificareTermeni(
+    ListTerms,  % propozitie completa
+    [JSDataKBOriginal | JSDataKB],  % knowledge base-ul
+    JSDataInput,    % json-ul citit la input
+    OutDictionary   % dictionarul final cu rezultatele
+) :- 
+    % verificam sa fie dictionar
+    \+ string(JSDataKB),
+
+    get_dict(Key, JSDataKB, UnKBranch), % scoatem termenii din dictionar
+    \+ getFirstLetter(Key, '_'), % sa ignoram flag-urile
+
+    % are flag-ul de _canAbsent
+    get_dict('_canAbsent', UnKBranch, _),
+    unificareTermeni(ListTerms, [JSDataKBOriginal | UnKBranch], JSDataInput, OutDictionary)
 .
 
 % am ajuns la final
@@ -49,64 +119,10 @@ unificareTermeni(
     % check if it is final
     \+ string(JSDataKB),
     get_dict('_isFinal', JSDataKB, _),
+    !, % oprim recursivitatea
 
     % generam dictionarul cu rezultatele
-    dict_create(OutputDict, _, []),
-    ! % oprim recursivitatea, am gasit solutia
-.
-
-% s-a terminat input-ul
-unificareTermeni(
-    [HProp], % parcurgerea popozitiei
-    _, % knowledge base-ul
-    _, % json-ul citit la input
-    OutputDict % dictionarul final cu rezultatele
-) :-
-    % verificam daca este un singur cuvant
-    string(HProp),
-    
-    % generam un dictionar
-    dict_create(TempDict, _, []),
-    put_dict('@[ERROR_001]', TempDict, HProp, OutputDict),
-    ! % oprim recursivitatea
-.
-
-unificareTermeni(
-    [HProp|TProp],  % parcurgerea popozitiei
-    [JSDataKBOriginal | JSDataKB],  % knowledge base-ul
-    JSDataInput,  % json-ul citit la input
-    OutDictionary  % dictionarul final cu rezultatele
-) :- 
-    % scoatem datele din dictionar
-    atom_string(Atom, HProp), get_dict(Atom, JSDataKB, Termen), % scoatem termenii din dictionar
-    
-    % daca nu este sub-arbore, incepem din root
-    string(Termen),
-    !, % oprire recursivitate
-
-    % scoatem din root primul termen
-    atom_string(RootAtom, Termen),
-    get_dict(RootAtom, JSDataKBOriginal, RootBranch), % scoatem termenii din dictionar
-
-    % reincepem parcurgerea din root
-    unificareTermeni(TProp, [JSDataKBOriginal | RootBranch], JSDataInput, OutDictionary)
-.
-
-unificareTermeni(
-    [HProp|TProp],  % parcurgerea popozitiei
-    [JSDataKBOriginal | JSDataKB],  % knowledge base-ul
-    JSDataInput,    % json-ul citit la input
-    OutDictionary    % dictionarul final cu rezultatele
-) :- 
-    % scoatem datele din dictionar
-    atom_string(Atom, HProp), get_dict(Atom, JSDataKB, Termen), % scoatem termenii din dictionar
-    
-    % mergem pe ramura aceasta
-    unificareTermeni(TProp, [JSDataKBOriginal | Termen], JSDataInput, InDictionary),
-
-    % verificam daca este variabila
-    % incepe cu @[cuvant]
-    completeDictionary(HProp, JSDataKB, JSDataInput, InDictionary, OutDictionary)
+    dict_create(OutputDict, _, [])
 .
 
 % analizam json-ul primit
@@ -136,6 +152,7 @@ readJson(InputFile, JSData) :-
     json_read_dict(Stream, JSData)
 .
 
+% ./NLID/KernelModule/
 % entryPoint
 entryPoint(OutputDict) :-
     readJson("./NLID/KernelModule/Input.txt", JSData), % citi
